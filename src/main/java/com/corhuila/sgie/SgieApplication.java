1a5dd2b487508473ddc6eeefd836e2746a47cede
sebastianzambrano <jszambranop@gmail.com>
2025-09-09 23:07:13 -0500

    feat: creacion del proyecto y estructura principal

---
+++ src/main/java/com/corhuila/sgie/SgieApplication.java
@@ -0,0 +1,13 @@
+package com.corhuila.sgie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/corhuila/sgie/SgieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corhuila.sgie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
